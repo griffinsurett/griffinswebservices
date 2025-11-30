84ba9a8ad00e68ac937ce75d6048c01b8666d7c1
griffinjoshs <leveloutusa@gmail.com>
2025-11-29 21:56:59 -0500

    no language switcher

--- src/components/ThemeControls/checkboxes/CircleCheckbox.tsx
+++ src/components/ThemeControls/checkboxes/CircleCheckbox.tsx
@@ -22,7 +22,7 @@ export function CircleCheckbox({
       />
 
       <span
-        className={`${className} w-7 h-7 lg:w-9 lg:h-9 rounded-full transition-all flex items-center justify-center relative`}
+        className={`${className} w-8 h-8 lg:w-9 lg:h-9 rounded-full transition-all flex items-center justify-center relative`}
       >
         {children}
       </span>
